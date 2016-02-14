9520173a5382f0f5337c43d8a4d3ddc42082bbed
Dani Sancas <lord.sancas@gmail.com>
2016-02-14 17:49:16 +0100

    Class implementation and nearly half-covered by tests
    Signed-off-by: Dani Sancas <lord.sancas@gmail.com>

--- soxta/soxta.py
+++ soxta/soxta.py
@@ -1,11 +1,33 @@
 #!/usr/bin/python
+# coding=utf-8
 
+import ast
+import calendar
+import iso8601
 import avro.schema
 
 from sys import argv
 from os.path import isfile
+from lxml import etree
 from avro.datafile import DataFileWriter
-from avro.io import DatumWriter
+from avro.io import DatumWriter, json
+
+
+class AttributeNotExpectedException(Exception):
+    def __init__(self, schema, attribute, row=None):
+        pretty_schema = json.dumps(json.loads(str(schema)), indent=2)
+        if row:
+            self.message = "Attribute {} from row {} is not expected in schema {}"\
+                           .format(attribute, str(row), pretty_schema)
+        else:
+            self.message = "Attribute {} is not expected in schema {}".format(attribute, pretty_schema)
+
+
+class FieldTypeNotExpectedException(Exception):
+    def __init__(self, schema, field_type, types_list):
+        pretty_schema = json.dumps(json.loads(str(schema)), indent=2)
+        self.message = "Field type {} from {} list not expected. Schema dump: {}"\
+                       .format(field_type, types_list, pretty_schema)
 
 
 class XmlToAvro:
@@ -18,6 +40,67 @@ class XmlToAvro:
             self.xmlfile = xmlfile
         self.schema = avro.schema.parse(open(avroschema).read())
         self.writer = DataFileWriter(open(outputavro, "w"), DatumWriter(), self.schema, codec="snappy")
+        self.schema_fields = {}  # Init expected fields for the schema
+        self.list_fields()
+
+    def list_fields(self):
+        for field in self.schema.fields:
+            self.schema_fields[field.name] = self.field_types_list(field.type)
+
+    def transform(self):
+        """Reads the XML row by row, transforms data to AVRO and deletes last row from memory"""
+        context = etree.iterparse(self.xmlfile, tag='row')
+        for event, element in context:
+            self.writer.append(self.process_xml_row(element))
+            element.clear()
+            while element.getprevious() is not None:
+                del element.getparent()[0]
+        del context
+
+    def process_xml_row(self, element):
+        """For each row, iterate attributes and add key-value pairs to datum. Then return datum"""
+        datum = {}
+        for attribute_name, attribute_value in element.attrib.items():
+            for field in self.schema.fields:
+                if field.name == attribute_name:
+                    available_types_list = self.schema_fields[field.name]  # Gets all available types for given field
+                    datum[attribute_name] = self.field_to_datum(available_types_list, attribute_value)
+            else:
+                raise AttributeNotExpectedException(self.schema, field, element)
+        return datum
+
+    @staticmethod
+    def field_types_list(field_type):
+        """Simplifies list by always returning a list, whether is a string, or a list"""
+        if isinstance(field_type, avro.schema.UnionSchema):  # Is a list
+            return [str(schematype.props["type"]).lower() for schematype in field_type.schemas]
+        else:  # Is a single field
+            return [str(field_type.type).lower()]
+
+    def field_to_datum(self, types_list, attribute_value):
+        """Iterates over available types list and returns attribute value in the proper format"""
+        for item_type in types_list:
+            if item_type == "string":
+                return str(attribute_value)
+            elif item_type == "int":
+                return int(attribute_value)
+            elif item_type == "boolean":
+                return ast.literal_eval(attribute_value.capitalize())
+            elif item_type == "long":
+                if "T" in attribute_value:  # It's an iso8601 date
+                    return int(calendar.timegm(iso8601.parse_date(attribute_value).timetuple()))
+                else:
+                    return long(attribute_value)
+            elif item_type == "array":
+                if "&lt;" in attribute_value or "&gt;" in attribute_value:  # Tag array
+                    without_start = attribute_value.replace("&lt;", "")
+                    split_string = without_start.split("&gt;")
+                    split_string.remove("")  # Possible remaining empty string
+                    return split_string
+            elif item_type == "null":
+                pass  # Do nothing
+            else:
+                FieldTypeNotExpectedException(self.schema, item_type, types_list)  # Schema contains non expected field
 
 
 if __name__ == "__main__":
--- test/test_soxta.py
+++ test/test_soxta.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# coding=utf-8
 
 import unittest
 from soxta.soxta import XmlToAvro
@@ -6,11 +7,31 @@ from soxta.soxta import XmlToAvro
 
 class TestXmlToAvro(unittest.TestCase):
 
-    def test_XmlToAvro_class_creation_not_enough_params(self):
+    def setUp(self):
+        """Setup XmlToAvro sample object"""
+        xmlfile = "data/Sample.xml"
+        avroschema = "data/Sample.avsc"
+        outputavro = "data/Sample.avro"
+        self.xta = XmlToAvro(xmlfile, avroschema, outputavro)  # It should give no error
+
+    def test_XmlToAvro_class_creation_no_params(self):
         """Not enough parameters provided"""
         self.assertRaises(TypeError, XmlToAvro)
+
+    def test_XmlToAvro_class_creation_one_param(self):
+        """Not enough parameters provided"""
         self.assertRaises(TypeError, XmlToAvro, "")
+
+    def test_XmlToAvro_class_creation_two_params(self):
+        """Not enough parameters provided"""
         self.assertRaises(TypeError, XmlToAvro, "", "")
+
+    def test_XmlToAvro_class_creation_pass(self):
+        """Set path to files"""
+        xmlfile = "data/Sample.xml"
+        avroschema = "data/Sample.avsc"
+        outputavro = "data/Sample.avro"
+        XmlToAvro(xmlfile, avroschema, outputavro)  # It should give no error
 
     def test_XmlToAvro_class_creation_non_existing_xml(self):
         """Set wrong path to files"""
@@ -26,9 +47,179 @@ class TestXmlToAvro(unittest.TestCase):
         outputavro = "data/Sample.avro"
         self.assertRaises(IOError, XmlToAvro, xmlfile, avroschema, outputavro)
 
-    def test_XmlToAvro_class_creation_pass(self):
-        """Set path to files"""
+    def test_field_to_datum_string_from_string(self):
+        """Evaluates if given string attribute value matches string"""
+        types_list = ["string"]
+        self.assertIsInstance(self.xta.field_to_datum(types_list, "something"), str)
+
+    def test_field_to_datum_string_from_int(self):
+        """Evaluates if given integer attribute value matches string"""
+        types_list = ["string"]
+        self.assertIsInstance(self.xta.field_to_datum(types_list, 15), str)
+
+    def test_field_to_datum_string_from_positive_int(self):
+        """Evaluates if given integer attribute value matches string"""
+        types_list = ["string"]
+        self.assertEqual(self.xta.field_to_datum(types_list, 15), "15")
+
+    def test_field_to_datum_string_from_negative_int(self):
+        """Evaluates if given negative integer attribute value matches string"""
+        types_list = ["string"]
+        self.assertEqual(self.xta.field_to_datum(types_list, -15), "-15")
+
+    def test_field_to_datum_string_from_boolean_true(self):
+        """Evaluates if given boolean attribute value matches string"""
+        types_list = ["string"]
+        self.assertIsInstance(self.xta.field_to_datum(types_list, True), str)
+
+    def test_field_to_datum_string_from_boolean_true_string(self):
+        """Evaluates if given boolean attribute value matches boolean string"""
+        types_list = ["string"]
+        self.assertEqual(self.xta.field_to_datum(types_list, True), "True")
+
+    def test_field_to_datum_string_from_boolean_false(self):
+        """Evaluates if given boolean attribute value matches string"""
+        types_list = ["string"]
+        self.assertIsInstance(self.xta.field_to_datum(types_list, False), str)
+
+    def test_field_to_datum_string_from_boolean_false_string(self):
+        """Evaluates if given boolean attribute value matches booleanstring"""
+        types_list = ["string"]
+        self.assertEqual(self.xta.field_to_datum(types_list, False), "False")
+
+    def test_field_to_datum_positive_int_from_positive_int(self):
+        """Evaluates if given integer attribute value matches integer"""
+        types_list = ["int"]
+        self.assertIsInstance(self.xta.field_to_datum(types_list, 15), int)
+
+    def test_field_to_datum_positive_int_value_from_positive_int(self):
+        """Evaluates if given integer attribute value matches integer"""
+        types_list = ["int"]
+        self.assertEqual(self.xta.field_to_datum(types_list, 15), 15)
+
+    def test_field_to_datum_negative_int_from_negative_int(self):
+        """Evaluates if given negative integer attribute value matches integer"""
+        types_list = ["int"]
+        self.assertIsInstance(self.xta.field_to_datum(types_list, -15), int)
+
+    def test_field_to_datum_negative_int_value_from_negative_int(self):
+        """Evaluates if given negative integer attribute value matches integer"""
+        types_list = ["int"]
+        self.assertEqual(self.xta.field_to_datum(types_list, -15), -15)
+
+    def test_field_to_datum_positive_int_from_number_string(self):
+        """Evaluates if given attribute value matches integer"""
+        types_list = ["int"]
+        self.assertIsInstance(self.xta.field_to_datum(types_list, "15"), int)
+
+    def test_field_to_datum_negative_int_from_number_string(self):
+        """Evaluates if given attribute value matches integer"""
+        types_list = ["int"]
+        self.assertIsInstance(self.xta.field_to_datum(types_list, "-15"), int)
+
+    def test_field_to_datum_int_from_non_number_string(self):
+        """Evaluates if given attribute value matches integer"""
+        types_list = ["int"]
+        self.assertRaises(ValueError, self.xta.field_to_datum, types_list, "something")
+
+    def test_field_to_datum_boolean_from_true_capitalized_string(self):
+        """Evaluates if given true capitalized attribute value matches boolean true"""
+        types_list = ["boolean"]
+        self.assertTrue(self.xta.field_to_datum(types_list, "True"))
+
+    def test_field_to_datum_boolean_from_false_capitalized_string(self):
+        """Evaluates if given false capitalized attribute value matches boolean false"""
+        types_list = ["boolean"]
+        self.assertFalse(self.xta.field_to_datum(types_list, "False"))
+
+    def test_field_to_datum_boolean_from_true_lower_string(self):
+        """Evaluates if given true lower attribute value matches boolean"""
+        types_list = ["boolean"]
+        self.assertTrue(self.xta.field_to_datum(types_list,  "true"))
+
+    def test_field_to_datum_boolean_from_false_lower_string(self):
+        """Evaluates if given false lower attribute value matches boolean"""
+        types_list = ["boolean"]
+        self.assertFalse(self.xta.field_to_datum(types_list, "false"))
+
+    def test_field_to_datum_boolean_from_true_upper_string(self):
+        """Evaluates if given true upper attribute value matches boolean"""
+        types_list = ["boolean"]
+        self.assertTrue(self.xta.field_to_datum(types_list, "TRUE"))
+
+    def test_field_to_datum_boolean_from_false_upper_string(self):
+        """Evaluates if given false upper attribute value matches boolean"""
+        types_list = ["boolean"]
+        self.assertFalse(self.xta.field_to_datum(types_list, "FALSE"))
+
+    def test_field_to_datum_boolean_from_non_boolean_string(self):
+        """Evaluates if given string attribute value matches boolean"""
+        types_list = ["boolean"]
+        self.assertRaises(ValueError, self.xta.field_to_datum, types_list, "something")
+
+    def test_field_to_datum_boolean_from_non_boolean_int(self):
+        """Evaluates if given integer attribute value matches boolean"""
+        types_list = ["boolean"]
+        self.assertRaises(AttributeError, self.xta.field_to_datum, types_list, 15)  # In 1º place, can't be capitalized
+
+    def test_field_to_datum_long_from_datetime_string(self):
+        """Evaluates if given datetime string matches long"""
+        types_list = ["long"]
+        self.assertEqual(self.xta.field_to_datum(types_list, "2008-08-01T05:12:44.193"), 1217567564)
+
+    def test_field_to_datum_long_from_long_string(self):
+        """Evaluates if given long string matches long"""
+        types_list = ["long"]
+        self.assertEqual(self.xta.field_to_datum(types_list, "1217567564"), 1217567564)
+
+    def test_field_to_datum_long_from_string(self):
+        """Evaluates if given long string matches long"""
+        types_list = ["long"]
+        self.assertRaises(ValueError, self.xta.field_to_datum, types_list, "something")
+
+    def test_field_to_datum_array_from_string(self):
+        """Evaluates if given long string matches long"""
+        types_list = ["array"]
+        result = ['c#', 'winforms', 'type-conversion', 'decimal', 'opacity']
+        self.assertEqual(self.xta.field_to_datum(types_list,
+                         "&lt;c#&gt;&lt;winforms&gt;&lt;type-conversion&gt;&lt;decimal&gt;&lt;opacity&gt;"), result)
+
+    def test_list_fields_from_default_schema(self):
+        expected_dict = {'Id': ["int"],
+                         'TagName': ["string", "null"],
+                         'Count': ["int"],
+                         'ExcerptPostId': ["int", "null"],
+                         'WikiPostId': ["int", "null"]}
+
+        self.xta.list_fields()
+        self.assertEqual(self.xta.schema_fields, expected_dict)
+
+    def test_list_fields_from_schema_with_arrays(self):
+        expected_dict = {'AcceptedAnswerId': ["int", "null"],
+                         'AnswerCount': ["int", "null"],
+                         'Body': ["string"],
+                         'ClosedDate': ["long", "null"],
+                         'CommentCount': ["int"],
+                         'CommunityOwnedDate': ["long", "null"],
+                         'CreationDate': ["long"],
+                         'FavoriteCount': ["int", "null"],
+                         'Id': ["int"],
+                         'LastActivityDate': ["long"],
+                         'LastEditDate': ["long", "null"],
+                         'LastEditorDisplayName': ["string", "null"],
+                         'LastEditorUserId': ["int", "null"],
+                         'OwnerDisplayName': ["string", "null"],
+                         'OwnerUserId': ["int", "null"],
+                         'ParentId': ["int", "null"],
+                         'PostTypeId': ["int"],
+                         'Score': ["int"],
+                         'Tags': ["null", "array"],
+                         'Title': ["string", "null"],
+                         'ViewCount': ["int", "null"]}
+
         xmlfile = "data/Sample.xml"
-        avroschema = "data/Sample.avsc"
+        avroschema = "data/Sample2.avsc"
         outputavro = "data/Sample.avro"
-        XmlToAvro(xmlfile, avroschema, outputavro)  # It should give no error
+        complex_xta = XmlToAvro(xmlfile, avroschema, outputavro)
+        complex_xta.list_fields()
+        self.assertEqual(complex_xta.schema_fields, expected_dict)
